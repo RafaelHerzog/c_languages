34decf1e8dca0ee1ef0fceaac140f86ed8eb8342
Rafael Herzog de Oliveira <rafaelvys@gmail.com>
2020-04-11 16:30:52 -0300

    testando

--- C#/teste.cs
+++ C#/teste.cs
@@ -1,68 +1,75 @@
 ﻿using System;
 
-namespace teste
+public class Person
 {
-    public class Person
+    public string name;
+
+    public int age;
+
+    private char gender;
+
+    public Person(){}
+
+    public Person(string name, int age, char gender)
     {
-        public string name;
-        public int age;
-        private char gender;
-
-        public Person(){}
-
-        public Person(string name, int age, char gender)
-        {
-            this.name = name;
-            this.age = age;
-            this.setGender(gender);
-        }
-
-        public override string ToString()
-        {
-            return "Name: " + this.name +
-                    "\nAge: " + this.age +
-                    "\nGender: " + this.getGender() + '\n';
-        }
-
-        public string getGender()
-        {
-            if (this.gender == 'F')
-                return "Female";
-            else if (this.gender == 'M')
-                return "Male";
-
-            return "Undefined";
-        }
-
-        public void setGender(char gender)
-        {
-            gender = Char.ToUpper(gender);
-
-            if(gender != 'F' && gender != 'M')
-                gender = 'N';
-            
-            this.gender = gender;
-        }
+        this.name = name;
+        this.age = age;
+        this.setGender(gender);
     }
-    class Program
+
+    public override string ToString()
     {
-        static void Main(string[] args)
-        {   
-            Person p = new Person();
+        return "Name: " +
+        this.name +
+        "\nAge: " +
+        this.age +
+        "\nGender: " +
+        this.getGender() +
+        '\n';
+    }
 
-            Console.Write("Enter your name: ");
-            p.name = Console.ReadLine();
+    public string getGender()
+    {
+        if (this.gender == 'F')
+            return "Female";
+        else if (this.gender == 'M') return "Male";
 
-            Console.Write("Enter your age: ");
-            p.age = Convert.ToInt32(Console.ReadLine());
+        return "Undefined";
+    }
 
-            Console.Write("Enter your gender: ");
-            char gender = Convert.ToChar(Console.ReadLine());
-            p.setGender(gender);
+    public void setGender(char gender)
+    {
+        gender = Char.ToUpper(gender);
 
-            Console.WriteLine("\nYour name is " + p.name + " and you're " + p.age + " years old\n");
+        if (gender != 'F' && gender != 'M') gender = 'N';
 
-            Console.WriteLine(p.ToString());
-        }
+        this.gender = gender;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person();
+
+        Console.Write("Enter your name: ");
+        p.name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        p.age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter your gender: ");
+        char gender = Convert.ToChar(Console.ReadLine());
+        p.setGender (gender);
+
+        Console
+            .WriteLine("\nYour name is " +
+            p.name +
+            " and you're " +
+            p.age +
+            " years old\n");
+
+        Console.WriteLine(p.ToString());
     }
 }
